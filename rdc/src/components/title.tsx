b303ed83efd17289b0496a76a0c2ca3a6d4ad2a3
Thien Nguyen <thien.x.nguyen20@gmail.com>
2023-01-02 23:31:32 -0700

    updated subtitle

--- rdc/src/components/title.tsx
+++ rdc/src/components/title.tsx
@@ -1,18 +1,16 @@
 // A title component that says "Rent Distribution Calculator" and a subtitle that says "Calculate how much each roommate should pay"
-import * as React from 'react';
-import '../styles/Title.css';   
+import * as React from "react";
+import "../styles/Title.css";
 
-
-
-const Title: React.FC = () => 
-{
-    return (
-        <div>
-            <h1 className="title">Rent Distribution Calculator</h1>
-            <h2 className="subtitle">Calculate how much each roommate should pay</h2>
-        </div>
-    );
+const Title: React.FC = () => {
+  return (
+    <div>
+      <h1 className="title">Rent Distribution Calculator</h1>
+      <h2 className="subtitle">
+        Calculate how much each roommate should pay (Based on Salary)
+      </h2>
+    </div>
+  );
 };
 
 export default Title;
-
